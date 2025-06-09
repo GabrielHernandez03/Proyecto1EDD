25dff5ad9087dcd87363acecd38746678aa9c24d
GabrielHernandez03 <gabriel-hernandez@correo.unimet.edu.ve>
2025-06-09 18:29:39 -0400

    Colas

--- src/NodoCola.java
+++ src/NodoCola.java
@@ -7,12 +7,43 @@
  *
  * @author gabriel
  */
- 
 public class NodoCola<E> {
+
+    public NodoLista<E> primero;
+    public NodoLista<E> ultimo;
     public int size;
+
     public NodoCola() {
+        primero = null;
+        ultimo = null;
         size = 0;
     }
 
+    public void encolar(NodoLista<E> nodo) {
+        if (isEmpty()) {
+            primero = nodo;
+        } else {
+            ultimo.next = nodo;
+        }
+        ultimo = nodo;
+        size++;
+    }
+
+    public NodoLista<E> desencolar() {
+        if (isEmpty()) {
+            return null;
+        }
+        NodoLista<E> desencolado = primero;
+        primero = primero.next;
+        if (primero == null) {
+            ultimo = null;
+        }
+        size--;
+        return desencolado;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
 }
