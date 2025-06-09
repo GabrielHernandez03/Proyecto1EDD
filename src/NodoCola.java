11eb187211bef9be00110082d239eb3e47573a83
GabrielHernandez03 <gabriel-hernandez@correo.unimet.edu.ve>
2025-06-09 15:11:38 -0400

    NodoCola

---
+++ src/NodoCola.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author gabriel
+ */
+ 
+public class NodoCola<E> {
+    public int size;
+    public NodoCola() {
+        size = 0;
+    }
+
+
+}
