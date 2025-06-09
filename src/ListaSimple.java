c87692bd4a0bbeade6aec286fe0be36bf666218a
GabrielHernandez03 <gabriel-hernandez@correo.unimet.edu.ve>
2025-06-09 17:47:29 -0400

    ListaSimple

---
+++ src/ListaSimple.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author gabriel
+ */
+public class ListaSimple<E> {
+
+    protected NodoLista<E> head;
+
+    /**
+     * Constructs empty list.
+     */
+    public ListaSimple() {
+        this.head = null;
+    }
+}
