d6363dd0defbb5c4c1a517cf2f0edab740018a82
GabrielHernandez03 <gabriel-hernandez@correo.unimet.edu.ve>
2025-06-19 01:18:14 -0400

    Grafo

---
+++ src/Proyecto1HernandezGabriel.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author gabriel
+ */
+public class Proyecto1HernandezGabriel {
+
+    /**
+     * Creates a SopaDeLetras MainFrame window.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame window = new MainFrame();
+    }
+    
+}
